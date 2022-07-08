3cfd49a6da140a7fde23f6d02e67656eb01c015d
enit#9747 <93931228+enit9747@users.noreply.github.com>
2022-07-08 03:49:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+    <title>enit.club</title>
+    <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/stylesheet.css" />
+        
+    <meta name="description" content="enit.club - gaming software" />
+<span class="type-it" style="color:#00ff00;"></span>
+</div>
+<a href="http://www.enit.club/"><big>back</a></big>
+<br>
+<br>
+<br>
+<br>
+<br>
+<body>
+ <big><p style="color:#FF0000";>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Public Cracks:</p>
+</big></body>
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    <a href="https://cdn.discordapp.com/attachments/858108963778199552/954547011556884511/skyrant.exe"><big>skyrant [valorant]</a ></big> <br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    <a href="https://cdn.discordapp.com/attachments/858108963778199552/954545247269040158/euphoria.rar"><big>euphoria [rust]</a></big> <br>
+    
+<style type="text/css">
+			
+			* {
+				margin: 0;
+				padding: 0;
+			}
+			body {
+
+			}
+			.background-wrap {
+				position: fixed;
+				z-index: -1000;
+				width: 100%;
+				height: 100%;
+				overflow: hidden;
+				top: 0;
+				left: 0;
+			}
+			div {                    /* this will hide all divs on the page */
+            display:none;
+            }
+			#video-bg-elem {
+				position: absolute;
+				top: 0;
+				left: 0;
+				min-height: 100%;
+				min-width: 100%;
+			}
+			
+		</style>
+<style>
+body{
+background-color:Black
+}
+	
